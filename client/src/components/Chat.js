8f91ee928c136ae71920e3783e81f05b379b63bb
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-05-13 17:54:00 +0400

    wrap Chat in authHOC

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PeopleList from "../containers/PeopleList";
 import MessagesList from "../containers/MessagesList";
+import authHOC from "../decorators/authHOC"
 
-export default class Chat extends React.Component {
+class Chat extends React.Component {
     render() {
         return (
             <div className="container clearfix">
@@ -12,3 +13,5 @@ export default class Chat extends React.Component {
         )
     }
 }
+
+export default authHOC(Chat)
