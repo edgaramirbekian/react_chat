3fbb6e9af5144c0ad5045ba7a0dd0463bab84108
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-05-13 17:52:48 +0400

    fix ws export

--- client/src/util/ws.js
+++ client/src/util/ws.js
@@ -1,7 +1,26 @@
-((wsUrl) => {
+export default ((wsUrl) => {
     let ws = new WebSocket(wsUrl)
     ws.onopen = () => {
         console.log("WS Open")
     }
-    return ws
+
+    ws.onmessage = (message) => {
+        console.log(message.data);
+
+    }
+
+    let countReconnect = 0
+    const emit = (message) => {
+        if (countReconnect > 5) return
+        if (ws.readyState === ws.CONNECTING) {
+            setTimeout(() => {
+                emit(message)
+                countReconnect++
+            }, 500)
+            return
+        }
+        ws.send(message)
+        countReconnect = 0
+    }
+    return { emit }
 })('ws://localhost:4000')
