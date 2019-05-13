4ea9546577660d3d8ad640f47d7d035925f7123a
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-05-14 02:42:13 +0400

    remove user when disconnected

--- client/src/reducers/people.js
+++ client/src/reducers/people.js
@@ -1,13 +1,19 @@
 import constants from "../constants/index";
 
 const peopleReducer = (state = [], action) => {
-    if (action.type === constants.NEW_USER_CONNECTED) {
-        return state.concat({
-            userName: action.userName,
-            id: action.userID
-        })
+    switch (action.type) {
+        case constants.NEW_USER_CONNECTED:
+            return state.concat({
+                userName: action.userName,
+                id: action.userID
+            })
+        case constants.DISCONNECTED_USER:
+            return state.filter((user) => {
+                return user.id !== action.userID
+            })
+        default:
+            return state
     }
-    return state
 }
 
 export default peopleReducer
