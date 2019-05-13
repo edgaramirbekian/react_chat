7b1ce8c5ab553a0456b01bc733fd70661e06b050
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-05-13 17:53:44 +0400

    create authentication Higher Order Component

---
+++ client/src/decorators/authHOC.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ws from '../util/ws'
+
+export default (ChildComponent) => {
+    return class authHoc extends React.Component {
+        auth() {
+            if (localStorage.getItem('auth')) return true
+            let name = prompt("Enter your name: ")
+            if (!name || !name.trim().length) {
+                return false
+            }
+            localStorage.setItem('auth', name)
+            ws.emit(name)
+            return true
+        }
+
+        noName() {
+            return (
+                <div className="noname-wrap">
+                    You didn't enter name :(
+                <br />
+                    <button onClick={() => { window.location.reload() }}>Try Again</button>
+                </div>
+            )
+        }
+        render() {
+            console.log(ws);
+
+            return (
+                this.auth() ? <ChildComponent /> : this.noName()
+            )
+        }
+    }
+}
