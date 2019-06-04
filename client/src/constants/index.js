d224380e33d831ddda3042e59316784d2d8237c0
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-06-04 15:22:20 +0400

    add receive msg constant

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -2,5 +2,7 @@ import keyMirror from "key-mirror";
 
 export default keyMirror({
     NEW_USER_CONNECTED: null,
-    DISCONNECTED_USER: null
+    DISCONNECTED_USER: null,
+    RECEIVE_NEW_MESSAGE: null,
+    ADD_NEW_MESSAGE: null
 })
