05aba445c4d150480c79e8b78b668277f486fd0e
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-06-04 15:21:56 +0400

    remove image

--- client/src/components/UserItem.js
+++ client/src/components/UserItem.js
@@ -5,7 +5,6 @@ export default class UserItem extends React.Component {
 
         return (
             <li className="clearfix">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg" alt="avatar" />
                 <div className="about">
                     <div className="name">{this.props.userName}</div>
                     <div className="status">
