de252956246fab39a936e94888acec155f13ec3d
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-06-04 15:24:39 +0400

    add message reducer

--- client/src/reducers/messages.js
+++ client/src/reducers/messages.js
@@ -1,5 +1,19 @@
-const messagesReducer = (state = {}, action) => {
-    return state
+import constants from "../constants/index";
+
+const messagesReducer = (state = [], action) => {
+    console.log(action);
+
+    switch (action.type) {
+        case constants.RECEIVE_NEW_MESSAGE:
+            return state.concat({
+                author: action.author,
+                text: action.text,
+                time: action.time,
+                color: action.color
+            })
+        default:
+            return state
+    }
 }
 
 export default messagesReducer
